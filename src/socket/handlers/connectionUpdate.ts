9daba51dfa4700c8563cac2017ac2b28814ce557
Cézar Bezerra <cezar30santos@gmail.com>
2025-05-25 19:23:45 -0300

    chore: Implements reconection

--- src/socket/handlers/connectionUpdate.ts
+++ src/socket/handlers/connectionUpdate.ts
@@ -1,6 +1,7 @@
 import { Boom } from "@hapi/boom";
 import { DisconnectReason } from "baileys";
 import QRCode from "qrcode";
+import { startSocket } from "../socket";
 
 export async function handleConnectionUpdate(update: any, sock: any) {
   const { connection, lastDisconnect, qr } = update;
@@ -9,12 +10,29 @@ export async function handleConnectionUpdate(update: any, sock: any) {
     console.log(await QRCode.toString(qr, { type: "terminal", small: true }));
   }
 
-  if (
-    connection === "close" &&
-    (lastDisconnect?.error as Boom)?.output?.statusCode ===
+  if (connection === "close") {
+    if (
+      (lastDisconnect?.error as Boom)?.output?.statusCode ===
       DisconnectReason.restartRequired
-  ) {
-    console.log("⚠️ Socket restart required, reconnecting...");
-    require("../socket").startSocket(); // reinicia conexão
+    ) {
+      console.log("⚠️ Socket restart required, reconnecting...");
+      startSocket(); // reinicia conexão
+    }
+
+    if (
+      (lastDisconnect?.error as Boom)?.output?.statusCode ===
+      DisconnectReason.loggedOut
+    ) {
+      console.log(
+        "🚪 Desconectado permanentemente. Reautenticação necessária."
+      );
+    } else {
+      console.log("🔄 Conexão encerrada. Tentando reconectar...");
+      startSocket(); // sua função que reinicia o socket
+    }
+  }
+
+  if (connection === "open") {
+    console.log("✅ Conectado ao WhatsApp");
   }
 }
